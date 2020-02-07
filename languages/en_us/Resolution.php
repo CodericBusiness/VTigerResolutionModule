c6d6bbf603cb1019bd74a92d18e9e7350d02905b
Neftali Yagua <despacho@neftali.yagua.com.ve>
2020-02-07 17:10:06 -0500

    Agregando módulo terminado

---
+++ languages/en_us/Resolution.php
@@ -0,0 +1,29 @@
+<?php
+/***********************************************************************************************
+** The contents of this file are subject to the Vtiger Module-Builder License Version 1.3
+ * ( "License" ); You may not use this file except in compliance with the License
+ * The Original Code is:  Technokrafts Labs Pvt Ltd
+ * The Initial Developer of the Original Code is Technokrafts Labs Pvt Ltd.
+ * Portions created by Technokrafts Labs Pvt Ltd are Copyright ( C ) Technokrafts Labs Pvt Ltd.
+ * All Rights Reserved.
+**
+*************************************************************************************************/
+
+$languageStrings = Array(
+	'Resolution' => 'Resolution',
+	'SINGLE_Resolution' => 'Resolution',
+	'ModuleName ID' => 'Resolution ID',
+	
+	'LBL_ADD_RECORD' => 'Add Resolution',
+	'LBL_RECORDS_LIST' => 'Resolution List',
+	
+	'LBL_CUSTOM_INFORMATION' => 'Custom Information',
+	'LBL_MODULEBLOCK_INFORMATION' => 'ModuleBlock Information',
+
+	'ModuleFieldLabel' => 'ModuleFieldLabel Text',
+
+	
+
+);
+	
+?>
---
+++ languages/es_es/Resolution.php
@@ -0,0 +1,29 @@
+<?php
+/***********************************************************************************************
+** The contents of this file are subject to the Vtiger Module-Builder License Version 1.3
+ * ( "License" ); You may not use this file except in compliance with the License
+ * The Original Code is:  Technokrafts Labs Pvt Ltd
+ * The Initial Developer of the Original Code is Technokrafts Labs Pvt Ltd.
+ * Portions created by Technokrafts Labs Pvt Ltd are Copyright ( C ) Technokrafts Labs Pvt Ltd.
+ * All Rights Reserved.
+**
+*************************************************************************************************/
+
+$languageStrings = Array(
+	'Resolution' => 'Resoluciones',
+	'SINGLE_Resolution' => 'Resolución',
+	'Resolution ID' => 'Resolución ID',
+	
+	'LBL_ADD_RECORD' => 'Nueva Resolución',
+	'LBL_RECORDS_LIST' => 'Lista de Resoluciones',
+	
+	'LBL_CUSTOM_INFORMATION' => 'Custom Information',
+	'LBL_MODULEBLOCK_INFORMATION' => 'ModuleBlock Information',
+
+	'ModuleFieldLabel' => 'ModuleFieldLabel Text',
+
+	
+
+);
+	
+?>
